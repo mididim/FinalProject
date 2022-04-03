98515396166932c1732426f1e442e1b646edb54f
Dimitry <mididim@gmail.com>
2022-04-03 14:01:23 +0300

    online starter

---
+++ src/main/java/lv/sda/finalproject/model/User.java
@@ -0,0 +1,62 @@
+package lv.sda.finalproject.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private UserRole role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
